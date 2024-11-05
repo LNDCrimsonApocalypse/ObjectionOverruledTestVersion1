d12499df8e11fa5bc3d28a6862b9068c30f49f31
FlamerYeeter <86110168+FlamerYeeter@users.noreply.github.com>
2024-11-05 10:58:02 +0800

    Script Added For Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BtnStart()
+    {
+        SceneManager.LoadScene("CaseReview-RoleSelectionScreen");
+    }
+
+    /*    public void BtnRestart()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }*/
+    public void BtnOption()
+    {
+        SceneManager.LoadScene("SettingsScreen");
+    }
+    public void BtnTutorial()
+    {
+        SceneManager.LoadScene("TutorialScreen");
+    }
+
+    public void BtnAbout()
+    {
+        SceneManager.LoadScene("AboutScreen");
+    }
+
+    public void BtnQuit()
+    {
+        Application.Quit();
+    }
+
+}
